672a9ffbb32260046812161908f971d1937e5953
Isaiah Reiff <realmackreiff@gmail.com>
2025-11-11 23:32:10 -0500

    DB Controller and Route work

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({ path: '../.env' });
-
+require('dotenv').config({ path: '../.env_example' });
 
 
 const app = express();

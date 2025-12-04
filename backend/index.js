bba894d344b9be17bf9b452fdcf50953cba97f41
Gareth Noble <garethnoble3@gmail.com>
2025-12-04 18:50:23 -0500

    Added Config For Accepted CORS Request.

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,19 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 
 
-// Middleware
-app.use(cors());
+// Allows Use Of Following Connections.
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://flashcard-project-frontend-kzltgwfvv-garethnoblelfgs-projects.vercel.app',
+    /^https:\/\/flashcard-project-frontend-.*\.vercel\.app$/
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+})); 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

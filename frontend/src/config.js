e6d8ee9341780fefd760bc565dfacd699b46d522
Gareth Noble <garethnoble3@gmail.com>
2025-12-04 20:52:23 -0500

    Another Edit To Config.js Disrupting Connection To Backend.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,3 @@
-require('dotenv').config();   //{ path: '../.env' } Use This For Local Dev.
-
-const API_URL = process.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 export default API_URL;

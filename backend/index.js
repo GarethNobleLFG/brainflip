547d8fbc2e45a34d3f618063a334d6ffa1f6b732
Gareth Noble <garethnoble3@gmail.com>
2025-12-04 20:38:29 -0500

    Fixed File Path For .env File For Vercel.

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();   //{ path: '../.env' } Use This For Local Dev.
 
 
 const app = express();
